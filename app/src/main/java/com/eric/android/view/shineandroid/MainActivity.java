1d2506489640c755c4b2e48bb6ce2a8094d61120
Eric <yuanshuaiding@163.com>
2018-01-03 18:18:46 +0800

    Initial commit

---
+++ app/src/main/java/com/eric/android/view/shineandroid/MainActivity.java
@@ -0,0 +1,25 @@
+package com.eric.android.view.shineandroid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.eric.android.view.shine.ShineFrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final ShineFrameLayout fl_shine = findViewById(R.id.fl_shine);
+        fl_shine.setTilt(30);
+        fl_shine.setAngle(ShineFrameLayout.MaskAngle.CW_210);
+        fl_shine.startShimmerAnimation();
+        fl_shine.setOnClickListener(v -> {
+            if (fl_shine.isAnimationStarted())
+                fl_shine.stopShimmerAnimation();
+            else
+                fl_shine.startShimmerAnimation();
+        });
+    }
+}
